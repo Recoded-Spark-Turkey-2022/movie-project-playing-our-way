65e0ce428870b5d5ecd5204acaaa9abe89a52b82
SeymaDemir <seymademir35@gmail.com>
2022-11-13 21:55:21 +0300

    fetched related films about chosen film

--- script.js
+++ script.js
@@ -5,8 +5,14 @@ const PROFILE_BASE_URL = "http://image.tmdb.org/t/p/w185";
 const BACKDROP_BASE_URL = "http://image.tmdb.org/t/p/w780";
 const CONTAINER = document.querySelector(".container");
 
+
+//get actors
 // TMD_BASE_URL / movie/{movieid} / credits?api-key=xxx & language=en-US
 //https://api.themoviedb.org/3/ movie/436270 /credits?api_key=542003918769df50083a13c415bbc602&language=en-US
+
+//get similar films
+// TMD_BASE_URL  / movie/{movieid} / similar?api-key=xxx & language=en-US
+//https://api.themoviedb.org/3/movie/436270/similar?api_key=542003918769df50083a13c415bbc602&language=en-US&page=1
 
 // Don't touch this function please
 const autorun = async () => {
@@ -27,7 +33,8 @@ const constructUrl = (path) => {
 const movieDetails = async (movie) => {
   const movieRes = await fetchMovie(movie.id);
   const actorRes = await fetchActors(movie.id);
-  renderMovie(movieRes, actorRes);
+  const relatedFilms = await fetchRelatedFilms(movie.id);
+  renderMovie(movieRes, actorRes, relatedFilms);
 };
 
 // This function is to fetch movies. You may need to add it or change some part in it in order to apply some of the features.
@@ -43,6 +50,13 @@ const fetchActors = async (id) => {
   //console.log(res.json())
   return res.json();
 };
+
+const fetchRelatedFilms = async (id) => {
+  const url = constructUrl(`movie/${id}/similar`);
+  const res = await fetch(url);
+  //console.log(res.json())
+  return res.json();
+}
 
 // Don't touch this function please. This function is to fetch one movie.
 const fetchMovie = async (movieId) => {
@@ -68,7 +82,7 @@ const renderMovies = (movies) => {
 };
 
 // You'll need to play with this function in order to add features and enhance the style.
-const renderMovie = (movie, actors) => {
+const renderMovie = (movie, actors, relatedFilms) => {
   CONTAINER.innerHTML = `
     <div class="row">
         <div class="col-md-4">
@@ -87,24 +101,48 @@ const renderMovie = (movie, actors) => {
         </div>
         </div>
             <h3>Actors:</h3>
-            <ul id="actors" class="list-unstyled">
+            <ul id="actors" class="list-unstyled" >
+        
+            </ul>
+        </div>
+        <div>
+        <h3>Similar Films:</h3>
+            <ul id="similarFilms" class="list-unstyled">
+            
             </ul>
         </div>`;
-  const actorList = document.getElementById("actors")
-  actorList.append(renderActors(actors))
+ renderActors(actors)
+ renderSimilarFilms(relatedFilms)
+
 };
 
+//this function provides to get main 5 actor informations about each film
 const renderActors = (actors) =>  {
+  const actorList = document.querySelector("#actors") 
     actors.cast.slice(0, 5).map((actor) => {
     const actorDiv = document.createElement("ul");
     actorDiv.innerHTML = `
         <li>${actor.name}</li>
         <img src="${BACKDROP_BASE_URL + actor.profile_path}" alt="${actor.name} poster">`;
     actorDiv.addEventListener("click", () => {displaySingleActorPage();});
-    CONTAINER.appendChild(actorDiv);
+    actorList.appendChild(actorDiv);
+  });
+}
+
+
+//this function provides to get 5 related films about the chosen film
+const renderSimilarFilms = (similarFilms) =>  {
+  const relatedFilmsList = document.querySelector("#similarFilms")
+    similarFilms.results.slice(0, 5).map((film) => {
+    const filmDiv = document.createElement("ul");
+    filmDiv.innerHTML = `
+        <li>${film.original_title}</li>
+        <img src="${BACKDROP_BASE_URL + film.poster_path}" alt="${film.title} poster">`;
+    ///actorDiv.addEventListener("click", () => {displaySingleActorPage();});
+    relatedFilmsList.appendChild(filmDiv);
   });
 
-
+}
   const displaySingleActorPage = () => {
     CONTAINER.innerHTML = `
       <div class="row">
@@ -112,6 +150,6 @@ const renderActors = (actors) =>  {
                <h1>welcome, you are in actor page</h1>
           </div>`;
   };
-}
+
 
 document.addEventListener("DOMContentLoaded", autorun);
