bdddf08ed672770c9a9945b3e361dc926b515875
Ahmad Al Hariri <ahmadmhd357@gmail.com>
2022-11-14 16:43:54 +0300

    creating the temporary navbar

--- script.js
+++ script.js
@@ -153,3 +153,81 @@ const renderSimilarFilms = (similarFilms) =>  {
 
 
 document.addEventListener("DOMContentLoaded", autorun);
+const navEl = document.createElement('nav');
+navEl.innerHTML = `<div class="navbar">
+<i class='bx bx-menu'></i>
+<div class="logo"><a href="#">MOVIES</a></div>
+<div class="nav-links">
+  <div class="sidebar-logo">
+    <span class="logo-name">CodingLab</span>
+    <i class='bx bx-x' ></i>
+  </div>
+  <ul class="links">
+    <li><a href="#">HOME</a></li>
+    <li>
+      <a href="#">GENRES</a>
+      <i class='bx bxs-chevron-down htmlcss-arrow arrow  '></i>
+      <ul class="htmlCss-sub-menu sub-menu">
+        <li><a href="#">Action</a></li>
+        <li><a href="#">Romantic</a></li>
+        <li><a href="#">Drama</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#">FILTER</a>
+      <i class='bx bxs-chevron-down js-arrow arrow '></i>
+      <ul class="js-sub-menu sub-menu">
+        <li><a href="#">Most Popular</a></li>
+        <li><a href="#">New</a></li>
+        <li><a href="#">Good rated</a></li>
+        <li><a href="#">Example</a></li>
+      </ul>
+    </li>
+    <li><a href="#">ACTORS</a></li>
+    <li><a href="#">ABOUT US</a></li>
+  </ul>
+</div>
+<div class="search-box">
+  <i class='bx bx-search'></i>
+  <div class="input-box">
+    <input type="text" placeholder="Search...">
+  </div>
+</div>
+</div>`
+document.querySelector('body').prepend(navEl);
+ // search-box open close js code
+ let navbar = document.querySelector(".navbar");
+ let searchBox = document.querySelector(".search-box .bx-search");
+
+ 
+ searchBox.addEventListener("click", ()=>{
+   navbar.classList.toggle("showInput");
+   if(navbar.classList.contains("showInput")){
+     searchBox.classList.replace("bx-search" ,"bx-x");
+   }else {
+     searchBox.classList.replace("bx-x" ,"bx-search");
+   }
+ });
+ 
+ // sidebar open close js code
+ let navLinks = document.querySelector(".nav-links");
+ let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+ let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+ menuOpenBtn.onclick = function() {
+ navLinks.style.left = "0";
+ }
+ menuCloseBtn.onclick = function() {
+ navLinks.style.left = "-100%";
+ }
+ 
+ 
+//  sidebar submenu open close js code
+ let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+ htmlcssArrow.onclick = function() {
+  navLinks.classList.toggle("show1");
+ }
+
+ let jsArrow = document.querySelector(".js-arrow");
+ jsArrow.onclick = function() {
+  navLinks.classList.toggle("show3");
+ }
